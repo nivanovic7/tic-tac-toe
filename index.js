9bccf18444ecf61cfc06480f5d1ff589133421b4
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-07-15 14:23:09 +0200

    finshed styling buttons

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const cell = document.querySelectorAll(".cell");
 cell.forEach((el) => {
   el.addEventListener("click", function () {
     this.querySelector(".figure-x").style.display = "flex";
+    document.querySelectorAll(".next-playing .icons > *").forEach((el) => {
+      el.addEventListener("click", function () {
+        el.classList.toggle("show");
+      });
+    });
   });
 });
