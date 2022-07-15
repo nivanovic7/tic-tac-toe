7680c05ac6ecd21a949f66004d7d44e5945efdd5
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-07-15 14:39:44 +0200

    fininhed styleing button play again

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const cell = document.querySelectorAll(".cell");
+const ic = document.querySelectorAll(".next-playing .icons > div");
 
+console.log(ic);
 cell.forEach((el) => {
   el.addEventListener("click", function () {
     this.querySelector(".figure-x").style.display = "flex";
-    document.querySelectorAll(".next-playing .icons > *").forEach((el) => {
-      el.addEventListener("click", function () {
-        el.classList.toggle("show");
-      });
+    ic.forEach((el) => {
+      el.classList.toggle("show");
     });
   });
 });
