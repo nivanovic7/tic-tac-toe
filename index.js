a634bba05b3165e12db67854b5e5221ba53754d8
Nikola Ivanovic <nikola.ivanovic.ns@gmail.com>
2022-07-16 22:45:11 +0200

    added figure on message and evenlistener on modal quit btn

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const cell = document.querySelectorAll(".cell");
+const quitModal = document.querySelector(".quit");
 
 let isPlayingX = true;
+const xPlayerMoves = [];
+const oPlayerMoves = [];
+
+const showMessage = function () {
+  document.querySelector(".modal").classList.add("show");
+  const figureX = document.querySelector(".modal .icon-x");
+  const figureO = document.querySelector(".modal .icon-o");
+  isPlayingX ? figureX.classList.add("show") : figureO.classList.add("show");
+};
+
+const checkWin = function (moves) {
+  console.log(moves.includes("1"));
+  if (
+    (moves.includes(1) && moves.includes(2) && moves.includes(3)) ||
+    (moves.includes(4) && moves.includes(5) && moves.includes(6)) ||
+    (moves.includes(7) && moves.includes(8) && moves.includes(9)) ||
+    (moves.includes(1) && moves.includes(4) && moves.includes(7)) ||
+    (moves.includes(2) && moves.includes(5) && moves.includes(8)) ||
+    (moves.includes(3) && moves.includes(6) && moves.includes(9)) ||
+    (moves.includes(1) && moves.includes(5) && moves.includes(9)) ||
+    (moves.includes(3) && moves.includes(5) && moves.includes(8))
+  ) {
+    showMessage();
+  }
+};
+
 cell.forEach((el) => {
   el.addEventListener("click", function () {
     //display X or O depending on who is playing
-    isPlayingX
-      ? this.querySelector(".figure-x").classList.add("show")
-      : this.querySelector(".figure-o").classList.add("show");
+    if (isPlayingX) {
+      this.querySelector(".figure-x").classList.add("show");
+      xPlayerMoves.push(+this.dataset.id);
+      checkWin(xPlayerMoves);
+    } else {
+      this.querySelector(".figure-o").classList.add("show");
+      oPlayerMoves.push(+this.dataset.id);
+      checkWin(oPlayerMoves);
+    }
+
     isPlayingX = !isPlayingX;
     this.style.pointerEvents = "none";
 
@@ -15,4 +49,8 @@ cell.forEach((el) => {
       el.classList.toggle("show");
     });
   });
+});
+
+quitModal.addEventListener("click", function () {
+  document.querySelector(".modal").classList.remove("show");
 });
